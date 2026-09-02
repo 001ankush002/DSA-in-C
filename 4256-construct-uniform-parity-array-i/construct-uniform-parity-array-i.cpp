61adb9759026465f949684b78a6b42aa6d18dbb8
001ankush002 <ankushvishkarma311@gmail.com>
2026-09-02 20:30:45 +0530

    Time: 0 ms (100.00%) | Memory: 30.3 MB (36.43%) - LeetSync

---
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
@@ -0,0 +1,9 @@
+#include <vector>
+
+class Solution {
+public:
+    bool uniformArray(std::vector<int>& nums1) {
+        // The answer is always true for Part I due to the nature of parity transformations.
+        return true;
+    }
+};
